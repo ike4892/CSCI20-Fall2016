c56aba734f9321840a555a4d8d0df8f127613050
Kasey Koch <kkoch002@student.butte.edu>
2016-08-31 20:27:37 +0000

    lab 4 submission

---
+++ lab4/lab4.cpp
@@ -0,0 +1,36 @@
+  // Kasey Koch
+  // 8/29/2016
+  // souce http://www.projectlabyrinth.com/MadLibs/MadLibGenerated.php
+  // lab4
+  #include <iostream>
+  using namespace std;
+ 
+  
+   int main() 
+   {
+       string name = "" ;
+       char noun   ;
+       float number = 0 ;
+       string color = "" ;
+       string verb = "" ;
+       int number2 = 0 ;
+       
+        cout<< "insert for the following"<<endl;
+      cout<<"name"<<endl;
+      cin>>name;
+      cout <<"noun"<<endl;
+      cin>>noun;
+      cout<<"number"<<endl;
+      cin>>number;
+      cout<< "color"<<endl;
+      cin>>color;
+      cout<<"verb"<<endl;
+      cin>>verb;
+      cout<<"number2"<<endl;
+      cin>>number2;
+      
+      cout<<"Hey "<<name<< "will you be watching the" <<noun<< "tonight? Keep an eye out for the number " <<number<< "they're insane. It amazes me how the " <<color<< "ones " <<verb<< "like that. I hope next time it has more than " <<number2<< "rounds. "<<endl;
+        
+          
+       
+   }
