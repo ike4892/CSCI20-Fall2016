f4ef12ad05c6366ca09c6cd9d882252cc0ccdf25
Kasey Koch <kkoch002@student.butte.edu>
2016-08-31 20:28:14 +0000

    lab 2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,23 @@
+  #include <iostream>
+  using namespace std;
+  using std::cout;
+  
+   int main() {
+       int number = (0, 10)
+       cout<<"Guess a number between 0 and 10!"<<
+       
+       cin<<guess<<endl;
+       
+       if (number<guess)
+		cout<<"Too high try again"<<endl;
+       
+       
+       
+   };
+   int random(int min, int max) //range : [min, max)
+   
+   
+   //the psuedoCode Im actually supposed to have - make a user input a number preferabbly in a designated set such as 1 to 100, 
+   //then have it generate a number between that set
+   // have an if then statement to check whether or not the input number is higher, lower, or is the generated number
+   // display a text output of either higher, lower, or you win!!
