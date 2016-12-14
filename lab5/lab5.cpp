9e932d1dcb58a3aad2279be0280796413fb9eb56
Kasey Koch <kkoch002@student.butte.edu>
2016-12-14 17:37:10 +0000

    lab5 submission

---
+++ lab5/lab5.cpp
@@ -0,0 +1,35 @@
+// Kasey Koch
+// Date Created - 9/7/2016
+// Last Worked on - 9/7/2016
+// Lab 5
+
+
+  #include <iostream>
+  using namespace std;
+  
+  struct  monster{
+  int name;
+  int head;
+  int eyes;
+  int ears;
+  int nose;
+  int mouth;
+  };
+  
+  int main() {
+      monster OneMonster;
+      
+      OneMonster.name = OneMonster;
+      OneMonster.head = Zombus;
+      OneMonster.eyes = Spritem;
+      OneMonster.ears = Vegitas;
+      OneMonster.nose = None;
+      OneMonster.mouth = Wackus;
+
+  
+      cout<<monster.OneMonster<<endl;
+      
+      return 0;
+  }
+  
+  
